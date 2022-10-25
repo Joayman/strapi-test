93cb129148d3d9f6c63ab7b442880702dc9f28dd
Joseph Ayman <josephayman@mail.com>
2022-10-26 01:26:43 +0200

    Fetch API data

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from 'react'
+import useFetch from '../hooks/useFetch'
+import { Link } from 'react-router-dom'
 
 export default function Homepage() {
+    const { loading, error, data } = useFetch('http://localhost:1337/api/reviews')
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error :(</p>
+
+    console.log(data.data[0].attributes)
+
     return (
         <div>
-            <h1>Homepage</h1>
+            {data.data.map((reviews) => (
+                <div key={reviews.id} className="review-card">
+                    <div className="rating"><p>{reviews.attributes.Rating}</p></div>
+                    <h2>{reviews.attributes.Title}</h2>
+                    <small>console list</small>
+                    <Link to={`/details/${reviews.id}`}>Read more</Link>
+                </div>
+            ))}
         </div>
-    );
+    )
 }
